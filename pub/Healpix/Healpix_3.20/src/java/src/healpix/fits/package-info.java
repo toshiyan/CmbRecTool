7a7d698cb20dc145ec1e567041003ce65799b144
Your Name <you@example.com>
2017-07-13 15:03:38 -0700

    update

--- pub/Healpix/Healpix_3.20/src/java/src/healpix/fits/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains the fits utility classes. Only Gaia healpix fits implementation used is included here.
- *
- ** <img src="doc-files/package_overview.jpg">
- */
-@Deprecated
-package healpix.fits;
--- pub/Healpix/Healpix_3.20/src/java/src/healpix/tools/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains several tools for use within the HEALPix code. 
- * 
- ** <img src="doc-files/package_overview.jpg">
- */
-@Deprecated
-package healpix.tools;
