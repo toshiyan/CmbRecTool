8b819f5a8ee3b8bdd39bbf73c6351923998e5b76
Your Name <you@example.com>
2017-08-22 03:00:04 -0700

    update

--- src2/nldd/nldd_tau.f90
+++ src2/nldd/nldd_tau.f90
@@ -21,19 +21,21 @@ module nldd_tau
 contains
 
 
-subroutine Kernels_tau(rL,eL,WA,WB,X,kernel)
+subroutine Kernels_tau(rL,eL,WA,WB,X,kernel,gln,gle)
 !Kernels of lensing reconstruction noise
   implicit none
   !I/O
   character(*), intent(in) :: kernel
   integer, intent(in) :: rL(1:2), eL(1:2)
+  integer, intent(in), optional :: gln
+  double precision, intent(in), optional :: gle
   double precision, intent(in), dimension(rL(1):rL(2)) :: WA, WB
   double precision, intent(out) :: X(:)
   !internal
   type(gauss_legendre_params) :: GL
   character(LEN=4) :: selec
-  integer :: i, l
-  double precision :: pm, mu, Ip, Im, al, c1_inv, c3
+  integer :: i, l, gn
+  double precision :: pm, mu, Ip, Im, al, c1_inv, c3, ge
   double precision, dimension(2) :: d00_sup, d00_mid, d00_inf
   double precision, dimension(2) :: ZA00
   double precision, dimension(2) :: ZB00
@@ -49,7 +51,11 @@ subroutine Kernels_tau(rL,eL,WA,WB,X,kernel)
   X = 0d0
 
   !* GL quadrature
-  call gl_initialize(GL,int((3*max(rL(2),eL(2))+1)/2),1d-15)
+  gn = int((3*max(rL(2),eL(2))+1)/2)
+  ge = 1d-15
+  if (present(gln)) gn = gln
+  if (present(gle)) ge = gle
+  call gl_initialize(GL,gln,gle)
 
   do i = 1, GL%n !Gauss-Legendre Integration
     mu = GL%z(i)
@@ -93,31 +99,48 @@ subroutine Kernels_tau(rL,eL,WA,WB,X,kernel)
 end subroutine Kernels_tau
 
 
-subroutine AlTT_tau(rL,eL,Att,fC,OCTT,ITT)
+subroutine AlTT_tau(rL,eL,Att,fC,OCTT,ITT,gln,gle)
   implicit none
   !I/O
   integer, intent(in) :: rL(2), eL(2)
+  integer, intent(in), optional :: gln
+  double precision, intent(in), optional :: gle
   double precision, intent(in), dimension(:) :: fC
   double precision, intent(in), dimension(:), optional :: OCTT, ITT
   double precision, intent(out), dimension(:) :: Att
   !internal
+  integer :: gn, oL(2)
+  double precision :: ge
   double precision, dimension(rL(1):rL(2)) :: W1, W2
-  double precision, dimension(eL(2)) :: S0, G0
+  double precision, dimension(min(2*rL(2),eL(2))) :: S0, G0
 
   write(*,*) 'TT'
 
+  !only compute nonzero values
+  oL = [eL(1),min(2*rL(2),eL(2))]
+
+  !accuracy for GL
+  gn = int((3*max(rL(2),oL(2))+1)/2)
+  ge = 1d-15
+  if (present(gln)) gn = gln
+  if (present(gle)) ge = gle
+
+  !filtering functions
   if (present(OCTT)) W1 = 1d0 / OCTT(rL(1):rL(2))
   if (present(ITT))  W1 = ITT(rL(1):rL(2))
   if (.not.present(OCTT).and..not.present(ITT)) stop 'need OCTT or ITT'
+
+  !main calculation
   W2 = W1 * fC(rL(1):rL(2))**2
   S0 = 0d0
-  call Kernels_tau(rL,eL,W1,W2,S0,'S0')
+  call Kernels_tau(rL,oL,W1,W2,S0,'S0',gn,ge)
 
   W2 = W1 * fC(rL(1):rL(2))
   G0 = 0d0
-  call Kernels_tau(rL,eL,W2,W2,G0,'G0')
+  call Kernels_tau(rL,oL,W2,W2,G0,'G0',gn,ge)
 
-  Att(eL(1):eL(2)) = 1d0/(S0(eL(1):eL(2))+G0(eL(1):eL(2)))
+  Att = 0d0
+  Att(oL(1):oL(2)) = 1d0/(S0(oL(1):oL(2))+G0(oL(1):oL(2)))
 
 end subroutine AlTT_tau
 
