c2e4b2dacfb336fbbd29ac67bcdd8de0afcfc117
Your Name <you@example.com>
2019-03-01 03:00:13 -0800

    update

--- examples/nldd/al_fast/main.f90
+++ examples/nldd/al_fast/main.f90
@@ -8,11 +8,12 @@ program main
   use myutils,   only: linspace, savetxt
   use mycls,     only: readcl_camb
   use nldd_rot,  only: AaaEB
+  use nldd_tau,  only: AlTT_tau
   implicit none
   character(LEN=50) :: key(1:5)
   integer :: l, el(2), rL(2), oL(2)
   double precision, dimension(:,:), allocatable :: UC,LC,OC
-  double precision, dimension(:), allocatable :: Aaa,Nl
+  double precision, dimension(:), allocatable :: Aaa,Nl,Att
 
   call set_params_file
   call read_prm('rL',rL)
@@ -25,14 +26,15 @@ program main
   call readcl_camb(UC,read_str('ucl'),eL)
   call readcl_camb(LC,read_str('lcl'),eL,.true.)
 
-  allocate(Nl(eL(2)),OC(7,eL(2)),Aaa(oL(2)))
-  Nl = (read_dbl('sP')*ac2rad/Tcmb)**2*dexp(linspace(1,eL(2))*(linspace(1,eL(2))+1d0)*(read_dbl('fwhm')*ac2rad)**2/(8d0*dlog(2d0)))
-  OC(TT,:) = LC(TT,:) + Nl/2d0
-  OC(EE,:) = LC(EE,:) + Nl
-  OC(BB,:) = LC(BB,:)*(1d0-read_dbl('alpha')) + Nl
+  allocate(Nl(eL(2)),OC(7,eL(2)),Aaa(oL(2)),Att(oL(2)))
+  Nl = (read_dbl('sT')*ac2rad/Tcmb)**2*dexp(linspace(1,eL(2))*(linspace(1,eL(2))+1d0)*(read_dbl('fwhm')*ac2rad)**2/(8d0*dlog(2d0)))
+  OC(TT,:) = LC(TT,:) + Nl
+  OC(EE,:) = LC(EE,:) + 2d0*Nl
+  OC(BB,:) = LC(BB,:)*(1d0-read_dbl('alpha')) + 2d0*Nl
   call AaaEB(rL,oL,Aaa,LC(EE,:),OC(EE,:),OC(BB,:))
-  call savetxt('Aaa.dat',linspace(oL(1),oL(2),oL(2)-oL(1)+1),Aaa(oL(1):oL(2)),ow=.true.)
-  deallocate(LC,UC,OC,Aaa,Nl)
+  call AlTT_tau(rL,oL,Att,LC(TT,:),OC(TT,:))
+  call savetxt('Al.dat',linspace(oL(1),oL(2),oL(2)-oL(1)+1),Aaa(oL(1):oL(2)),Att(oL(1):oL(2)),ow=.true.)
+  deallocate(LC,UC,OC,Aaa,Att,Nl)
 
 end program main
 
--- examples/nldd/flat/main.f90
+++ examples/nldd/flat/main.f90
@@ -11,7 +11,7 @@ program main
   implicit none
   logical :: CV
   integer :: l, oL(2), eL(2), rL(2)
-  double precision, dimension(:), allocatable :: Nl, Ag, Ac
+  double precision, dimension(:), allocatable :: Nl, Ag, Ac, At, Ar
   double precision, dimension(:,:), allocatable :: UC, LC, W
 
   call set_params_file
@@ -29,20 +29,22 @@ program main
   !* experiment noise
   allocate(Nl(eL(2)))
   if(.not.CV) then
-    Nl = (read_dbl('sP')*ac2rad/Tcmb)**2*dexp(linspace(1,eL(2))*(linspace(1,eL(2))+1d0)*(read_dbl('fwhm')*ac2rad)**2/(8d0*dlog(2d0)))
+    Nl = (read_dbl('sT')*ac2rad/Tcmb)**2*dexp(linspace(1,eL(2))*(linspace(1,eL(2))+1d0)*(read_dbl('fwhm')*ac2rad)**2/(8d0*dlog(2d0)))
     do l = 2, eL(2)
-      W(2,l) = 1d0/(LC(EE,l)+Nl(l))
-      W(3,l) = 1d0/(LC(BB,l)*read_dbl('alpha')+Nl(l))
+      W(1,l) = 1d0/(LC(TT,l)+Nl(l))
+      W(2,l) = 1d0/(LC(EE,l)+Nl(l)*2d0)
+      W(3,l) = 1d0/(LC(BB,l)*read_dbl('alpha')+Nl(l)*2d0)
     end do
     !call savetxt('nlcl.dat',linspace(1,eL(2)),LC(BB,:),2d0*Nl,ow=.true.)
   end if
   deallocate(Nl)
 
   !Main Calculations
-  allocate(Ag(oL(2)),Ac(oL(2))); Ag=0d0
-  call alxy_flat('EB',rL,oL,Ag,Ac,LC(EE,:),W(2,:),W(3,:),weight='rotation')
-  call savetxt('al.dat',linspace(2,oL(2),oL(2)-1),Ag(2:),Ac(2:),ow=.true.)
-  deallocate(Ag,Ac,W,LC,UC)
+  allocate(Ag(oL(2)),Ac(oL(2)),At(oL(2)),Ar(oL(2))); Ag=0d0; Ac=0d0; At=0d0; Ar=0d0
+  call alxy_flat('TB',rL,oL,Ag,Ac,LC(EE,:),W(1,:),W(3,:),weight='rotation')
+  call alxy_flat('TT',rL,oL,At,Ar,LC(TT,:),W(1,:),W(1,:),weight='patchytau')
+  call savetxt('al.dat',linspace(2,oL(2),oL(2)-1),Ag(2:),Ac(2:),At(2:),Ar(2:),ow=.true.)
+  deallocate(Ag,Ac,At,Ar,W,LC,UC)
 
 end program main
 
